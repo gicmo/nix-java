a51c49b01f155f2ef1ca73f5d6d6569b7b9aba64
sujith <sujithvm24@gmail.com>
2015-05-28 01:10:08 +0530

    add constants for implementation mode

---
+++ nix/src/main/java/org/gnode/nix/Implementation.java
@@ -0,0 +1,12 @@
+package org.gnode.nix;
+
+/**
+ * NIX back-end implementations.
+ */
+public class Implementation {
+
+    /**
+     * HDF5 back-end implementation
+     */
+    public static final int Hdf5 = 0;
+}
