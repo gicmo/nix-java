ca6b32e1dfb10edc2129f6fc4e72d85724448c30
sujith <sujithvm24@gmail.com>
2015-05-28 01:08:17 +0530

    add constants for file open modes

---
+++ nix/src/main/java/org/gnode/nix/FileMode.java
@@ -0,0 +1,22 @@
+package org.gnode.nix;
+
+/**
+ * File open modes
+ */
+public class FileMode {
+
+    /**
+     * Read only mode
+     */
+    public static final int ReadOnly = 0;
+
+    /**
+     * Read, Write mode
+     */
+    public static final int ReadWrite = 1;
+
+    /**
+     * Overwrite mode
+     */
+    public static final int Overwrite = 2;
+}
