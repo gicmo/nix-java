cdbfb50bdb9d8add12ff1f16a99bf6ebc9ac4a76
sujith <sujithvm24@gmail.com>
2015-05-28 17:58:12 +0530

    add date conversion util funcs

--- nix/src/main/java/org/gnode/nix/internal/Utils.java
+++ nix/src/main/java/org/gnode/nix/internal/Utils.java
@@ -5,6 +5,7 @@ import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.javacpp.Pointer;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Utils {
 
@@ -56,5 +57,34 @@ public class Utils {
             }
         }
         return arrayList;
+    }
+
+    /**
+     * Convert seconds to appropriate date
+     * <p/>
+     * Required for functions that use time_t
+     *
+     * @param seconds seconds to be converted
+     * @return date object
+     */
+    public static Date convertSecondsToDate(long seconds) {
+        // convert to milliseconds by multiplying by 1000
+        // DateTime expects input in milliseconds
+        seconds *= 1000L;
+
+        return new Date(seconds);
+    }
+
+    /**
+     * Convert date object to seconds
+     * <p/>
+     * Required for functions that use time_t
+     *
+     * @param dateTime date to be converted
+     * @return seconds
+     */
+    public static long convertDateToSeconds(Date dateTime) {
+        long seconds = dateTime.getTime() / 1000;
+        return seconds;
     }
 }
