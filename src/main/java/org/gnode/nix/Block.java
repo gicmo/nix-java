de01e030f6ab71235844b482239c49a6c3d18798
sujith <sujithvm24@gmail.com>
2015-05-29 06:41:05 +0530

    add entity related funcs to Block

--- src/main/java/org/gnode/nix/Block.java
+++ src/main/java/org/gnode/nix/Block.java
@@ -2,8 +2,10 @@ package org.gnode.nix;
 
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.Pointer;
-import org.bytedeco.javacpp.annotation.Namespace;
-import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.*;
+import org.gnode.nix.internal.Utils;
+
+import java.util.Date;
 
 @Platform(value = "linux",
         include = {"<nix/Block.hpp>"},
@@ -25,5 +27,84 @@ public class Block extends Pointer {
     }
 
     private native void allocate();
+
+    //--------------------------------------------------
+    // Base class methods
+    //--------------------------------------------------
+
+    private native
+    @Cast("bool")
+    boolean isNone();
+
+    /**
+     * Checks if block is initialized
+     *
+     * @return true if initialized, False otherwise
+     */
+    public boolean isInitialized() {
+        return !isNone();
+    }
+
+    /**
+     * Get id of the block
+     *
+     * @return id string
+     */
+    public native
+    @Name("id")
+    @StdString
+    String getId();
+
+    private native
+    @Cast("time_t")
+    long createdAt();
+
+    /**
+     * Get the creation date of the block.
+     *
+     * @return The creation date of the block.
+     */
+    public Date getCreatedAt() {
+        return Utils.convertSecondsToDate(createdAt());
+    }
+
+    private native
+    @Cast("time_t")
+    long updatedAt();
+
+    /**
+     * Get the date of the last update.
+     *
+     * @return The date of the last update.
+     */
+    public Date getUpdatedAt() {
+        return Utils.convertSecondsToDate(updatedAt());
+    }
+
+    /**
+     * Sets the time of the last update to the current time if the field is not set.
+     */
+    public native void setUpdatedAt();
+
+    /**
+     * Sets the time of the last update to the current time.
+     */
+    public native void forceUpdatedAt();
+
+    /**
+     * Sets the creation time to the current time if the field is not set.
+     */
+    public native void setCreatedAt();
+
+    private native void forceCreatedAt(@Cast("time_t") long time);
+
+    /**
+     * Sets the creation date to the provided value even if the attribute is set.
+     *
+     * @param date The creation date to set.
+     */
+    public void forceCreatedAt(Date date) {
+        forceCreatedAt(Utils.convertDateToSeconds(date));
+    }
 
 }
