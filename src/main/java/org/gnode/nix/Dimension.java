38747038879eb00fed0636c9da1ab9a970f793e8
sujith <sujithvm24@gmail.com>
2015-05-30 14:05:00 +0530

    write basic dimension class

---
+++ src/main/java/org/gnode/nix/Dimension.java
@@ -0,0 +1,32 @@
+package org.gnode.nix;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.annotation.Namespace;
+import org.bytedeco.javacpp.annotation.Platform;
+
+@Platform(value = "linux",
+        include = {"<nix/Dimensions.hpp>"},
+        link = {"nix"})
+@Namespace("nix")
+public class Dimension extends Pointer {
+    static {
+        Loader.load();
+    }
+
+    //--------------------------------------------------
+    // Constructors
+    //--------------------------------------------------
+
+    /**
+     * Constructor that creates an uninitialized Dimension.
+     * <p/>
+     * Calling any method on an uninitialized dimension will throw a {@link java.lang.RuntimeException}.
+     */
+    public Dimension() {
+        allocate();
+    }
+
+    private native void allocate();
+
+}
