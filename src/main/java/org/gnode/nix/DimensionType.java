fd0b7523817adc2522bbd9f569a35c8a69ea6e1d
sujith <sujithvm24@gmail.com>
2015-05-30 08:45:52 +0530

    add dimension types

---
+++ src/main/java/org/gnode/nix/DimensionType.java
@@ -0,0 +1,20 @@
+package org.gnode.nix;
+
+public class DimensionType {
+
+    /**
+     * Sampled dimension
+     */
+    public static final int Sample = 0;
+
+    /**
+     * Set dimension
+     */
+    public static final int Set = 1;
+
+    /**
+     * Range dimension
+     */
+    public static final int Range = 2;
+
+}
