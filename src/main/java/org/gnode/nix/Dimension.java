fefe8d25870c0cdfbd627212dc50dacedc513575
sujith <sujithvm24@gmail.com>
2015-05-30 14:12:18 +0530

    implements methods for Dimension

--- src/main/java/org/gnode/nix/Dimension.java
+++ src/main/java/org/gnode/nix/Dimension.java
@@ -2,9 +2,7 @@ package org.gnode.nix;
 
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.Pointer;
-import org.bytedeco.javacpp.annotation.Cast;
-import org.bytedeco.javacpp.annotation.Namespace;
-import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.*;
 
 @Platform(value = "linux",
         include = {"<nix/Dimensions.hpp>"},
@@ -46,5 +44,63 @@ public class Dimension extends Pointer {
     public boolean isInitialized() {
         return !isNone();
     }
+
+    //--------------------------------------------------
+    // Methods concerning Dimension
+    //--------------------------------------------------
+
+    /**
+     * The actual dimension that is described by the dimension descriptor.
+     * <p/>
+     * The index of the dimension entity representing the dimension of the actual
+     * data that is defined by this descriptor.
+     *
+     * @return The dimension index of the dimension.
+     */
+    public native
+    @Name("index")
+    @Cast("size_t")
+    long getIndex();
+
+    /**
+     * The type of the dimension.
+     * <p/>
+     * This field indicates whether the dimension is a SampledDimension, SetDimension or
+     * RangeDimension.
+     *
+     * @return The dimension type.
+     */
+    public native
+    @Name("dimensionType")
+    @ByVal
+    @Cast("nix::DimensionType")
+    int getDimensionType();
+
+    /**
+     * Returns as {#link SetDimension}.
+     *
+     * @return SetDimension object
+     */
+    public native
+    @ByVal
+    SetDimension asSetDimension();
+
+    /**
+     * Returns as {#link SampledDimension}.
+     *
+     * @return SampledDimension object
+     */
+    public native
+    @ByVal
+    SampledDimension asSampledDimension();
+
+    /**
+     * Returns as {#link RangeDimension}.
+     *
+     * @return RangeDimension object
+     */
+    public native
+    @ByVal
+    RangeDimension asRangeDimension();
 
 }
