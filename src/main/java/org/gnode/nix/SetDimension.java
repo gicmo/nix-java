5cdd607334e1507c2eef4668ede84ea87a13dcca
sujith <sujithvm24@gmail.com>
2015-05-30 14:03:30 +0530

    add funcs for set dimension

--- src/main/java/org/gnode/nix/SetDimension.java
+++ src/main/java/org/gnode/nix/SetDimension.java
@@ -2,9 +2,8 @@ package org.gnode.nix;
 
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.Pointer;
-import org.bytedeco.javacpp.annotation.Cast;
-import org.bytedeco.javacpp.annotation.Namespace;
-import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.*;
+import org.gnode.nix.internal.None;
 
 @Platform(value = "linux",
         include = {"<nix/Dimensions.hpp>"},
@@ -45,5 +44,49 @@ public class SetDimension extends Pointer {
      */
     public boolean isInitialized() {
         return !isNone();
+    }
+
+    //--------------------------------------------------
+    // Methods concerning SetDimension
+    //--------------------------------------------------
+
+    /**
+     * The actual dimension that is described by the dimension descriptor.
+     * <p/>
+     * The index of the dimension entity representing the dimension of the actual
+     * data that is defined by this descriptor.
+     *
+     * @return The dimension index of the dimension.
+     */
+    public native
+    @Name("index")
+    @Cast("size_t")
+    long getIndex();
+
+    /**
+     * The type of the dimension.
+     * <p/>
+     * This field indicates whether the dimension is a SampledDimension, SetDimension or
+     * RangeDimension.
+     *
+     * @return The dimension type.
+     */
+    public native
+    @Name("dimensionType")
+    @ByVal
+    @Cast("nix::DimensionType")
+    int getDimensionType();
+
+    // public native @StdString @StdVector BytePointer labels();
+
+    // public native void labels(@StdString @StdVector BytePointer labels);
+
+    private native void labels(@Const @ByVal None t);
+
+    /**
+     * Remove the labels from the dimension.
+     */
+    public void removeLabels() {
+        labels(new None());
     }
 }
