2725cdae016fd7876d89e32a91bfffe2f7846219
sujith <sujithvm24@gmail.com>
2015-05-30 08:48:57 +0530

    add basic class for sampled dimension

---
+++ src/main/java/org/gnode/nix/SampledDimension.java
@@ -0,0 +1,31 @@
+package org.gnode.nix;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.annotation.Namespace;
+import org.bytedeco.javacpp.annotation.Platform;
+
+@Platform(value = "linux",
+        include = {"<nix/Dimensions.hpp>"},
+        link = {"nix"})
+@Namespace("nix")
+public class SampledDimension extends Pointer {
+    static {
+        Loader.load();
+    }
+
+    //--------------------------------------------------
+    // Constructors
+    //--------------------------------------------------
+
+    /**
+     * Constructor that creates an uninitialized SampledDimension.
+     * <p/>
+     * Calling any method on an uninitialized dimension will throw a {@link java.lang.RuntimeException}.
+     */
+    public SampledDimension() {
+        allocate();
+    }
+
+    private native void allocate();
+}
