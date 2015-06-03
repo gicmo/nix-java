30ed1e52db0d289f9944e834ead3b3c7daa3d22d
sujith <sujithvm24@gmail.com>
2015-06-03 23:36:11 +0530

    data type constants

---
+++ src/main/java/org/gnode/nix/DataType.java
@@ -0,0 +1,35 @@
+package org.gnode.nix;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.annotation.Namespace;
+import org.bytedeco.javacpp.annotation.Platform;
+
+@Platform(value = "linux",
+        include = {"<nix/DataType.hpp>"},
+        link = {"nix"})
+@Namespace("nix")
+public class DataType {
+
+    static {
+        Loader.load();
+    }
+
+    public static final int Bool = 0;
+    public static final int Char = 1;
+    public static final int Float = 2;
+    public static final int Double = 3;
+    public static final int Int8 = 4;
+    public static final int Int16 = 5;
+    public static final int Int32 = 6;
+    public static final int Int64 = 7;
+    public static final int UInt8 = 8;
+    public static final int UInt16 = 9;
+    public static final int UInt32 = 10;
+    public static final int UInt64 = 11;
+    public static final int String = 12;
+    public static final int Date = 13;
+    public static final int DateTime = 14;
+    public static final int Opaque = 15;
+
+    public static final int Nothing = -1;
+}
