627cfc8003512b17cce53cff64274170ff83fa8a
sujith <sujithvm24@gmail.com>
2015-06-09 18:13:10 +0530

    add link type constants

---
+++ src/main/java/org/gnode/nix/LinkType.java
@@ -0,0 +1,19 @@
+package org.gnode.nix;
+
+public class LinkType {
+
+    /**
+     * Link type - Tagged
+     */
+    public static final int Tagged = 0;
+
+    /**
+     * Link type - Untagged
+     */
+    public static final int Untagged = 1;
+
+    /**
+     * Link type - Indexed
+     */
+    public static final int Indexed = 2;
+}
