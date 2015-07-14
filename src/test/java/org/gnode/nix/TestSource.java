905dfc99b783b3b2233ffee640b1651f3c2bd8a7
sujith <sujithvm24@gmail.com>
2015-07-14 13:17:10 +0530

    [TestSource.java] update tests for findSources

--- src/test/java/org/gnode/nix/TestSource.java
+++ src/test/java/org/gnode/nix/TestSource.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +19,8 @@ public class TestSource {
     private Block block;
     private Section section;
     private Source source, source_other, source_null;
+    private DataArray darray;
+    private MultiTag mtag;
 
     private Date statup_time;
 
@@ -33,6 +36,17 @@ public class TestSource {
         source = block.createSource("source_one", "channel");
         source_other = block.createSource("source_two", "channel");
         source_null = null;
+
+        // create a DataArray & a MultiTag
+        darray = block.createDataArray("DataArray", "dataArray",
+                DataType.Double, new NDSize(new int[]{0, 0}));
+        double[] A = new double[5 * 5];
+        for (int i = 0; i < 5; i++) {
+            A[i * 5 + i] = 100 * i;
+        }
+        darray.setDataExtent(new NDSize(new int[]{5, 5}));
+        darray.setData(A, darray.getDataExtent(), new NDSize());
+        mtag = block.createMultiTag("tag_one", "test_tag", darray);
     }
 
     @After
@@ -155,6 +169,111 @@ public class TestSource {
         assertEquals(source.getSourceCount(), 0);
         assertEquals(source.getSources().size(), 0);
         assertFalse(source.hasSource("invalid_id"));
+    }
+
+    @Test
+    public void testFindSource() {
+        /* We create the following tree:
+         *
+         * source---l1n1---l2n1---l3n1
+         *    |      |      |
+         *    |      ------l2n2
+         *    |      |
+         *    |      |-----l2n3---l3n2
+         *    |             |
+         *    |             ------l3n3
+         *    ------l1n2
+         *    |
+         *    ------l1n3---l2n4
+         *           |
+         *           ------l2n5---l3n4
+         *           |
+         *           ------l2n6---l3n5
+         *                  |      |
+         * mtag-------------|      |
+         *                         |
+         * darray-------------------
+         */
+        Source l1n1 = source.createSource("l1n1", "typ1");
+        Source l1n2 = source.createSource("l1n2", "typ2");
+        Source l1n3 = source.createSource("l1n3", "typ3");
+        Source l2n1 = l1n1.createSource("l2n1", "typ1");
+        Source l2n2 = l1n1.createSource("l2n2", "typ2");
+        Source l2n3 = l1n1.createSource("l2n3", "typ2");
+        Source l2n4 = l1n3.createSource("l2n4", "typ2");
+        Source l2n5 = l1n3.createSource("l2n5", "typ2");
+        Source l2n6 = l1n3.createSource("l2n6", "typ3");
+        Source l3n1 = l2n1.createSource("l3n1", "typ1");
+        Source l3n2 = l2n3.createSource("l3n2", "typ2");
+        Source l3n3 = l2n3.createSource("l3n3", "typ2");
+        Source l3n4 = l2n5.createSource("l3n4", "typ2");
+        Source l3n5 = l2n5.createSource("l3n5", "typ2");
+        mtag.addSource(l2n6.getId());
+        darray.addSource(l3n5.getId());
+
+        // test if sources are in place
+        assertTrue(mtag.hasSource(l2n6));
+        assertTrue(darray.hasSource(l3n5));
+        assertTrue(mtag.getSources().size() == 1);
+        assertTrue(darray.getSources().size() == 1);
+
+        // test depth limit
+        assertTrue(source.findSources().size() == 15);
+        assertTrue(source.findSources((Source s) -> true, 2).size() == 10);
+        assertTrue(source.findSources((Source s) -> true, 1).size() == 4);
+        assertTrue(source.findSources((Source s) -> true, 0).size() == 1);
+
+        // test filter
+        Predicate<Source> filter_typ1 = (Source s) -> s.getType().equals("typ1");
+        Predicate<Source> filter_typ2 = (Source s) -> s.getType().equals("typ2");
+        assertTrue(source.findSources(filter_typ1).size() == 3);
+        assertTrue(source.findSources(filter_typ2).size() == 9);
+
+        // test deleter
+        /* chop the tree down to:
+         *
+         * source---l1n2
+         *    |
+         *    ------l1n3---l2n4
+         *           |
+         *           ------l2n5---l3n4
+         *           |
+         *           ------l2n6---l3n5
+         *                  |      |
+         * mtag-------------|      |
+         *                         |
+         * darray-------------------
+         */
+        source.deleteSource(l1n1.getId());
+        assertTrue(source.findSources().size() == 8);
+
+        /* chop the tree down to:
+         *
+         * source---l1n3---l2n4
+         *           |
+         *           ------l2n5---l3n4
+         *           |
+         *           ------l2n6---l3n5
+         *                  |      |
+         * mtag-------------|      |
+         *                         |
+         * darray-------------------
+         */
+        source.deleteSource(l1n2.getId());
+        assertTrue(source.findSources().size() == 7);
+
+        /* chop the tree down to:
+         *
+         * source
+         * mtag
+         * darray
+         */
+        source.deleteSource(l1n3.getId());
+        assertTrue(source.findSources().size() == 1);
+        assertFalse(mtag.hasSource(l2n6));
+        assertFalse(darray.hasSource(l3n5));
+        assertTrue(mtag.getSources().size() == 0);
+        assertTrue(darray.getSources().size() == 0);
     }
 
     @Test
