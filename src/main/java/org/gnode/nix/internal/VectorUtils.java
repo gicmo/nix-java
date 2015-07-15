030e5ac419e03fb5f622c17b3565a4b97b837430
sujith <sujithvm24@gmail.com>
2015-07-16 00:30:33 +0530

    [VectorUtils.java] update documentation

--- src/main/java/org/gnode/nix/internal/VectorUtils.java
+++ src/main/java/org/gnode/nix/internal/VectorUtils.java
@@ -11,6 +11,11 @@ import org.gnode.nix.valid.Message;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>VectorUtils</h1>
+ * Low level wrapper to std vectors.
+ */
+
 @Properties(value = {
         @Platform(include = {
                 "<vector>",
@@ -38,12 +43,22 @@ public class VectorUtils {
     // Block vector
     //--------------------------------------------------
 
+    /**
+     * <h1>BlockVector</h1>
+     * Low level <tt>std::vector&lt;nix::Block&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Block>")
     public static class BlockVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set blocks.
+         *
+         * @param lst list of blocks.
+         * @see Block
+         */
         public BlockVector(List<Block> lst) {
             allocate(lst.size());
 
@@ -62,6 +77,12 @@ public class VectorUtils {
 
         private native BlockVector put(@Cast("size_t") long i, Block block);
 
+        /**
+         * Get blocks.
+         *
+         * @return list of blocks.
+         * @see Block
+         */
         public List<Block> getBlocks() {
             ArrayList<Block> blocks = new ArrayList<Block>();
             for (int i = 0; i < size(); i++) {
@@ -75,12 +96,22 @@ public class VectorUtils {
     // DataArray vector
     //--------------------------------------------------
 
+    /**
+     * <h1>DataArrayVector</h1>
+     * Low level <tt>std::vector&lt;nix::DataArray&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::DataArray>")
     public static class DataArrayVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set data arrays.
+         *
+         * @param lst list of data arrays.
+         * @see DataArray
+         */
         public DataArrayVector(List<DataArray> lst) {
             allocate(lst.size());
 
@@ -99,6 +130,12 @@ public class VectorUtils {
 
         private native DataArrayVector put(@Cast("size_t") long i, DataArray dataArray);
 
+        /**
+         * Get data arrays.
+         *
+         * @return list of data arrays.
+         * @see DataArray
+         */
         public List<DataArray> getDataArrays() {
             ArrayList<DataArray> dataArrays = new ArrayList<DataArray>();
             for (int i = 0; i < size(); i++) {
@@ -112,12 +149,22 @@ public class VectorUtils {
     // Dimension vector
     //--------------------------------------------------
 
+    /**
+     * <h1>DimensionVector</h1>
+     * Low level <tt>std::vector&lt;nix::Dimension&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Dimension>")
     public static class DimensionVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set dimensions.
+         *
+         * @param lst list of dimensions.
+         * @see Dimension
+         */
         public DimensionVector(List<Dimension> lst) {
             allocate(lst.size());
 
@@ -136,6 +183,12 @@ public class VectorUtils {
 
         private native DimensionVector put(@Cast("size_t") long i, Dimension dimension);
 
+        /**
+         * Get dimensions.
+         *
+         * @return list of dimensions.
+         * @see Dimension
+         */
         public List<Dimension> getDimensions() {
             ArrayList<Dimension> dimensions = new ArrayList<Dimension>();
             for (int i = 0; i < size(); i++) {
@@ -149,12 +202,22 @@ public class VectorUtils {
     // Feature vector
     //--------------------------------------------------
 
+    /**
+     * <h1>FeatureVector</h1>
+     * Low level <tt>std::vector&lt;nix::Feature&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Feature>")
     public static class FeatureVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set features.
+         *
+         * @param lst list of features.
+         * @see Feature
+         */
         public FeatureVector(List<Feature> lst) {
             allocate(lst.size());
 
@@ -173,6 +236,12 @@ public class VectorUtils {
 
         private native FeatureVector put(@Cast("size_t") long i, Feature feature);
 
+        /**
+         * Get features.
+         *
+         * @return list of features.
+         * @see Feature
+         */
         public List<Feature> getFeatures() {
             ArrayList<Feature> features = new ArrayList<Feature>();
             for (int i = 0; i < size(); i++) {
@@ -186,12 +255,22 @@ public class VectorUtils {
     // MultiTag vector
     //--------------------------------------------------
 
+    /**
+     * <h1>MultiTagVector</h1>
+     * Low level <tt>std::vector&lt;nix::MultiTag&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::MultiTag>")
     public static class MultiTagVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set multi tags.
+         *
+         * @param lst list of multi tags.
+         * @see MultiTag
+         */
         public MultiTagVector(List<MultiTag> lst) {
             allocate(lst.size());
 
@@ -210,6 +289,12 @@ public class VectorUtils {
 
         private native MultiTagVector put(@Cast("size_t") long i, MultiTag multiTag);
 
+        /**
+         * Get multi tags.
+         *
+         * @return list of multi tags.
+         * @see MultiTag
+         */
         public List<MultiTag> getMultiTags() {
             ArrayList<MultiTag> multiTags = new ArrayList<MultiTag>();
             for (int i = 0; i < size(); i++) {
@@ -223,12 +308,22 @@ public class VectorUtils {
     // Property vector
     //--------------------------------------------------
 
+    /**
+     * <h1>PropertyVector</h1>
+     * Low level <tt>std::vector&lt;nix::Property&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Property>")
     public static class PropertyVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set properties.
+         *
+         * @param lst list of properties.
+         * @see Property
+         */
         public PropertyVector(List<Property> lst) {
             allocate(lst.size());
 
@@ -247,6 +342,12 @@ public class VectorUtils {
 
         private native PropertyVector put(@Cast("size_t") long i, Property property);
 
+        /**
+         * Get properties.
+         *
+         * @return list of properties.
+         * @see Property
+         */
         public List<Property> getProperties() {
             ArrayList<Property> properties = new ArrayList<Property>();
             for (int i = 0; i < size(); i++) {
@@ -260,12 +361,22 @@ public class VectorUtils {
     // Section vector
     //--------------------------------------------------
 
+    /**
+     * <h1>SectionVector</h1>
+     * Low level <tt>std::vector&lt;nix::Section&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Section>")
     public static class SectionVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set sections.
+         *
+         * @param lst list of sections.
+         * @see Section
+         */
         public SectionVector(List<Section> lst) {
             allocate(lst.size());
 
@@ -284,6 +395,12 @@ public class VectorUtils {
 
         private native SectionVector put(@Cast("size_t") long i, Section section);
 
+        /**
+         * Get sections.
+         *
+         * @return list of sections.
+         * @see Section
+         */
         public List<Section> getSections() {
             ArrayList<Section> sections = new ArrayList<Section>();
             for (int i = 0; i < size(); i++) {
@@ -297,12 +414,22 @@ public class VectorUtils {
     // Source vector
     //--------------------------------------------------
 
+    /**
+     * <h1>SourceVector</h1>
+     * Low level <tt>std::vector&lt;nix::Source&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Source>")
     public static class SourceVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set sources.
+         *
+         * @param lst list of sources.
+         * @see Source
+         */
         public SourceVector(List<Source> lst) {
             allocate(lst.size());
 
@@ -321,6 +448,12 @@ public class VectorUtils {
 
         private native SourceVector put(@Cast("size_t") long i, Source source);
 
+        /**
+         * Get source.
+         *
+         * @return list of sources.
+         * @see Source
+         */
         public List<Source> getSources() {
             ArrayList<Source> sources = new ArrayList<Source>();
             for (int i = 0; i < size(); i++) {
@@ -334,12 +467,22 @@ public class VectorUtils {
     // Tag vector
     //--------------------------------------------------
 
+    /**
+     * <h1>TagVector</h1>
+     * Low level <tt>std::vector&lt;nix::Tag&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Tag>")
     public static class TagVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set tags.
+         *
+         * @param lst list of tags.
+         * @see Tag
+         */
         public TagVector(List<Tag> lst) {
             allocate(lst.size());
 
@@ -358,6 +501,12 @@ public class VectorUtils {
 
         private native TagVector put(@Cast("size_t") long i, Tag tag);
 
+        /**
+         * Get tags.
+         *
+         * @return list of tags.
+         * @see Tag
+         */
         public List<Tag> getTags() {
             ArrayList<Tag> tags = new ArrayList<Tag>();
             for (int i = 0; i < size(); i++) {
@@ -371,12 +520,22 @@ public class VectorUtils {
     // Value vector
     //--------------------------------------------------
 
+    /**
+     * <h1>ValueVector</h1>
+     * Low level <tt>std::vector&lt;nix::Value&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::Value>")
     public static class ValueVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set values.
+         *
+         * @param lst list of values.
+         * @see Value
+         */
         public ValueVector(List<Value> lst) {
             allocate(lst.size());
 
@@ -395,6 +554,12 @@ public class VectorUtils {
 
         private native ValueVector put(@Cast("size_t") long i, Value value);
 
+        /**
+         * Get values.
+         *
+         * @return list of values.
+         * @see Value
+         */
         public List<Value> getValues() {
             ArrayList<Value> values = new ArrayList<Value>();
             for (int i = 0; i < size(); i++) {
@@ -408,12 +573,22 @@ public class VectorUtils {
     // Message vector
     //--------------------------------------------------
 
+    /**
+     * <h1>MessageVector</h1>
+     * Low level <tt>std::vector&lt;nix::valid::Message&gt;</tt> wrapper
+     */
     @Name("std::vector<nix::valid::Message>")
     public static class MessageVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set messages.
+         *
+         * @param lst list of messages.
+         * @see Message
+         */
         public MessageVector(List<Message> lst) {
             allocate(lst.size());
 
@@ -432,6 +607,12 @@ public class VectorUtils {
 
         private native MessageVector put(@Cast("size_t") long i, Message message);
 
+        /**
+         * Get messages.
+         *
+         * @return list of messages.
+         * @see Message
+         */
         public List<Message> getMessages() {
             ArrayList<Message> messages = new ArrayList<Message>();
             for (int i = 0; i < size(); i++) {
@@ -445,12 +626,22 @@ public class VectorUtils {
     // String vector
     //--------------------------------------------------
 
+    /**
+     * <h1>StringVector</h1>
+     * Low level <tt>std::vector&lt;std::string&gt;</tt> wrapper
+     */
     @Name("std::vector<std::string>")
     public static class StringVector extends Pointer {
         static {
             Loader.load();
         }
 
+        /**
+         * Set list of strings.
+         *
+         * @param lst list of strings.
+         * @see String
+         */
         public StringVector(List<String> lst) {
             allocate(lst.size());
 
@@ -469,6 +660,12 @@ public class VectorUtils {
 
         private native StringVector put(@Cast("size_t") long i, String value);
 
+        /**
+         * Get Strings.
+         *
+         * @return list of strings.
+         * @see String
+         */
         public List<String> getStrings() {
             ArrayList<String> strings = new ArrayList<String>();
             for (int i = 0; i < size(); i++) {
