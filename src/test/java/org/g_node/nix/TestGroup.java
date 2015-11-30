7f6d3129e1f1282a39f75de6fc282ad71052484b
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2015-11-30 11:24:58 +0100

    Group: add missing unit tests

--- src/test/java/org/g_node/nix/TestGroup.java
+++ src/test/java/org/g_node/nix/TestGroup.java
@@ -85,6 +85,9 @@ public class TestGroup {
         assertEquals(group.getSourceCount(), names.size());
         assertEquals(group.getSources().size(), names.size());
 
+        List<Source> filtered = group.getSources((s) -> s.getName().endsWith("a") || s.getName().endsWith("d"));
+        assertEquals(filtered.size(), 2);
+
         for (String id: ids) {
             Source s = group.getSource(id);
             assertEquals(s.getId(), id);
@@ -93,6 +96,107 @@ public class TestGroup {
 
         assertEquals(group.getSourceCount(), 0);
         assertEquals(group.getSources().size(), 0);
+    }
+
+    @Test
+    public void testDataArrayAccess() {
+        List<String> names = Arrays.asList("array_a", "array_b", "array_c", "array_d", "array_e");
+
+        assertEquals(group.getDataArrayCount(), 0);
+        assertTrue(group.getDataArrays().isEmpty());
+        assertFalse(group.hasDataArray(names.get(0)));
+
+        List<String> ids = new ArrayList<>();
+        for (String name: names) {
+            DataArray da = block.createDataArray(name, "some_type", DataType.Double, new NDSize(new int[]{0}));
+            ids.add(da.getId());
+            group.addDataArray(da);
+            assertEquals(group.getDataArray(name), da);
+        }
+
+        assertEquals(group.getDataArrayCount(), names.size());
+        assertEquals(group.getDataArrays().size(), names.size());
+
+        List<DataArray> filtered = group.getDataArrays((da) -> da.getName().endsWith("a") || da.getName().endsWith("d"));
+        assertEquals(filtered.size(), 2);
+
+        for (String id: ids) {
+            DataArray da = group.getDataArray(id);
+            assertEquals(da.getId(), id);
+            group.removeDataArray(da);
+        }
+
+        assertEquals(group.getDataArrayCount(), 0);
+        assertEquals(group.getDataArrays().size(), 0);
+    }
+
+    @Test
+    public void testTagAccess() {
+        List<String> tagNames = Arrays.asList("tag_a", "tag_b", "tag_c", "tag_d", "tag_e");
+
+        assertEquals(group.getTagCount(), 0);
+        assertTrue(group.getTags().isEmpty());
+        assertFalse(group.hasTag(tagNames.get(0)));
+
+        List<String> ids = new ArrayList<>();
+        for (String name: tagNames) {
+            DataArray da = block.createDataArray(name.replace("tag", "array"), "some_type", DataType.Double, new NDSize(new int[]{0}));
+            Tag tag = block.createTag(name, "some_type", new double[]{0});
+            tag.addReference(da);
+            ids.add(tag.getId());
+            group.addTag(tag);
+            assertEquals(group.getTag(name), tag);
+        }
+
+        assertEquals(group.getTagCount(), tagNames.size());
+        assertEquals(group.getTags().size(), tagNames.size());
+
+        List<Tag> filtered = group.getTags((tag) -> tag.getName().endsWith("a") || tag.getName().endsWith("d"));
+        assertEquals(filtered.size(), 2);
+
+        for (String id: ids) {
+            Tag tag = group.getTag(id);
+            assertEquals(tag.getId(), id);
+            group.removeTag(tag);
+        }
+
+        assertEquals(group.getTagCount(), 0);
+        assertEquals(group.getTags().size(), 0);
+    }
+
+    @Test
+    public void testMultiTagAccess() {
+        List<String> tagNames = Arrays.asList("tag_a", "tag_b", "tag_c", "tag_d", "tag_e");
+
+        assertEquals(group.getMultiTagCount(), 0);
+        assertTrue(group.getMultiTags().isEmpty());
+        assertFalse(group.hasMultiTag(tagNames.get(0)));
+
+        List<String> ids = new ArrayList<>();
+        for (String name: tagNames) {
+            DataArray da = block.createDataArray(name.replace("tag", "array"), "some_type", DataType.Double, new NDSize(new int[]{0}));
+            DataArray pos = block.createDataArray(name.replace("tag", "pos"), "some_type", DataType.Double, new NDSize(new int[]{0}));
+            MultiTag tag = block.createMultiTag(name, "some_type", pos);
+            tag.addReference(da);
+            ids.add(tag.getId());
+            group.addMultiTag(tag);
+            assertEquals(group.getMultiTag(name), tag);
+        }
+
+        assertEquals(group.getMultiTagCount(), tagNames.size());
+        assertEquals(group.getMultiTags().size(), tagNames.size());
+
+        List<MultiTag> filtered = group.getMultiTags((tag) -> tag.getName().endsWith("a") || tag.getName().endsWith("d"));
+        assertEquals(filtered.size(), 2);
+
+        for (String id: ids) {
+            MultiTag tag = group.getMultiTag(id);
+            assertEquals(tag.getId(), id);
+            group.removeMultiTag(tag);
+        }
+
+        assertEquals(group.getMultiTagCount(), 0);
+        assertEquals(group.getMultiTags().size(), 0);
     }
 
 }
